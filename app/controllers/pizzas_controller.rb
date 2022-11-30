ae056115c4ac464f0d11e057c415563d8d42b64d
Maureenkamau03 <mkaymaureen@gmail.com>
2022-11-30 05:52:10 +0300

    adding routes to pizza controller

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,2 +1,21 @@
 class PizzasController < ApplicationController
-end
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_method
+      # GET all 
+      def index
+          render json: Pizza.all, each_serializer: PizzaSerializer
+      end
+  
+      def show
+          pizza = Pizza.find_by(id: params[:id])
+          render json: pizza
+      end
+  
+      private
+  
+      def record_not_found
+          render json:{error: "Pizza not found"}, status: :not_found
+      end
+  end
+  
+  
