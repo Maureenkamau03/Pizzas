d6e55e078079717c387518482481e61e685ec5bf
Maureenkamau03 <mkaymaureen@gmail.com>
2022-11-30 05:19:41 +0300

    price validation

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,4 +1,15 @@
 class RestaurantPizza < ApplicationRecord
   belongs_to :pizza
   belongs_to :restaurant
+
+  validates :price, presence: true
+
+  validate :price_range
+
+    # custon price range validation(should be between 1 and 30)
+    def price_range
+        if (price > 30 || price < 1)
+            errors.add(:price, 'Must be between 30 and 1')
+        end            
+    end
 end
